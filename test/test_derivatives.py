39da466b9c2f2a6c25982981b9859a02599e9aa5
Pierre-Alain Fayolle <fayolle@u-aizu.ac.jp>
2021-04-14 12:15:01 +0900

    Take derivative wrt a parameter

---
+++ test/test_derivatives.py
@@ -0,0 +1,21 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+
+import torch
+
+import FRep
+from FRep.primitives import sphere
+from FRep.diff import grad
+
+
+x = torch.tensor([[1.0,0.0,0.0]])
+x.requires_grad = False
+center = torch.tensor([0.0,0.0,0.0])
+center.requires_grad = False
+r = torch.tensor([1.0])
+r.requires_grad = True
+
+f = sphere(x, center, r)
+dfdr = grad(f, r)
+print(dfdr)

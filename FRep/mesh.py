c0ec575a8a037bac5f4a8a9d4b815061213c68c9
Pierre-Alain Fayolle <fayolle@u-aizu.ac.jp>
2021-04-14 15:14:13 +0900

    Compute curvatures of implicit

--- FRep/mesh.py
+++ FRep/mesh.py
@@ -4,9 +4,6 @@ import torch
 from .grid import torchGrid, torchSampling
 
 
-# Vertices returned by marching_cubes are in [0,M]x[0,N]x[0,P]
-# where [M,N,P] are the dimensions of the volume grid. 
-# They should correspond instead to the geometric coordinates of the object
 def evalToMesh(model, grid_min, grid_max, grid_res, device='cpu'):
     x, y, z = torchGrid(grid_min, grid_max, grid_res, device)
     volume = torchSampling(model, x, y, z)
@@ -16,7 +13,7 @@ def evalToMesh(model, grid_min, grid_max, grid_res, device='cpu'):
     # where [M,N,P] are the dimensions of the volume grid. 
     # They should correspond instead to the geometric coordinates of the object
     vertices, faces, normals, _ = sk.marching_cubes(volume, level=0)
-
+    
     # vertices in [0,1]
     vertices[:,0] = vertices[:,0] / grid_res[0]
     vertices[:,1] = vertices[:,1] / grid_res[1]
---
+++ examples/mean_curvature.py
@@ -0,0 +1,37 @@
+'''
+Example to illustrate computing the mean curvature of an implicit surface 
+and rendering it. 
+'''
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+
+import torch
+
+import FRep
+from FRep.primitives import sphere
+from FRep.mesh import evalToMesh
+from FRep.curvature import meanCurvature, GaussianCurvature
+from FRep.IO import writeOFF
+
+import polyscope as ps
+
+
+def model(p):
+    sp1 = sphere(p, center=(0,0,0), r=2)
+    return sp1
+
+# Generate the mesh
+v,f,n = evalToMesh(model, grid_min=(-2,-2,-2), grid_max=(2,2,2), grid_res=(64,64,64))
+
+# Evaluate the mean curvature at the vertices
+Km = meanCurvature(model, v)
+Kg = GaussianCurvature(model, v)
+
+ps.init()
+ps.register_surface_mesh("mesh", v, f, smooth_shade=True)
+ps.get_surface_mesh("mesh").add_scalar_quantity("mean curvature", Km, defined_on='vertices', cmap='jet', enabled=True)
+ps.get_surface_mesh("mesh").add_scalar_quantity("Gaussian curvature", Kg, defined_on='vertices', cmap='jet', enabled=False)
+ps.show()
+

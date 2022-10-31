1fac25d649189a28b0c01dffbf45c5373b760322
Pierre-Alain Fayolle <fayolle@u-aizu.ac.jp>
2022-10-31 14:09:59 +0900

    Change the torus radii in the sawtooth example

--- examples/sawtooth_render.py
+++ examples/sawtooth_render.py
@@ -12,9 +12,9 @@ import polyscope as ps
 
 def replicant(p):
     q = sawtooth(p, 5.0)
-    torus1 = torusZ(q, (-1.0, 1.0, 0.0), 1.0, 0.1)
-    torus2 = torusX(q, (0.0, -1.0, -1.0), 1.0, 0.1)
-    torus3 = torusY(q, (1.0, 0.0, 1.0), 1.0, 0.1)
+    torus1 = torusZ(q, (-1.0, 1.0, 0.0), 1.0, 0.25)
+    torus2 = torusX(q, (0.0, -1.0, -1.0), 1.0, 0.25)
+    torus3 = torusY(q, (1.0, 0.0, 1.0), 1.0, 0.25)
     sp4 = sphere(q, (0.0, 0.0, 0.0), 0.8)
     res5 = boundBlendUnion(torus1, torus2, sp4, 0.08, 1.0, 1.0, 1.0)
     res6 = boundBlendUnion(res5, torus3, sp4, 0.08, 1.0, 1.0, 1.0)
